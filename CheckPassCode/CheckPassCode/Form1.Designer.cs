c2019b9cc978b380117fa6b2915cbd914d20f4d9
liangqian <liangqian0723@gmail.com>
2015-05-07 20:26:18 +0800

    现在只是运用最基本的GDI绘图来画出了一个随机小图而已，等过一段时间的时候，做图像处理的时候，再来完善

---
+++ CheckPassCode/CheckPassCode/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CheckPassCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(147, 123);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(147, 261);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 49);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Please Inpu Checkcode：";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 383);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 636);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CheckPassCode/CheckPassCode/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CheckPassCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string Mstr = null;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //先画字符
+            Random r = new Random();
+            string stri = null;
+            string strii = "ZXCVBNMLKJHGFDSAQWERTYUIP123456789";
+            for (int i = 0; i < 5; i++)
+            {
+                stri += strii[r.Next(0, 34)];
+            }
+            Mstr = stri;
+
+            Bitmap bmp = new Bitmap(120, 40);
+
+            Graphics g = Graphics.FromImage(bmp);
+
+            for (int i = 0; i < 5; i++)
+            {
+                string[] fonts = { "微软雅黑", "宋体", "隶书", "仿宋", "黑体" };
+                Color[] colors = { Color.Pink, Color.Black, Color.Red, Color.Brown, Color.Blue };
+                Point p = new Point(i * 20, 0);
+                g.DrawString(stri[i].ToString(), new Font(fonts[r.Next(0, 5)].ToString(), 20, FontStyle.Bold), new SolidBrush(colors[r.Next(0, 5)]), p);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Point p1 = new Point(r.Next(0, bmp.Width), r.Next(0, bmp.Height));
+                Point p2 = new Point(r.Next(0, bmp.Width), r.Next(0, bmp.Height));
+
+                g.DrawLine(new Pen(Brushes.SandyBrown), p1, p2);
+            }
+            for (int i = 0; i < 500; i++)
+            {
+                Point p = new Point(r.Next(0, bmp.Width), r.Next(0, bmp.Height));
+                bmp.SetPixel(p.X, p.Y, Color.CadetBlue);
+            }
+
+
+
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1_Click(sender, e);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (textBox1.Text.ToUpper() == Mstr)
+                {
+                    MessageBox.Show("Login Successfully！");
+                    pictureBox1_Click(sender,e);
+                }
+                else
+                {
+                    MessageBox.Show("Unfortunately，Your Input Incorrect！");
+                    pictureBox1_Click(sender,e);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Input Error");
+                pictureBox1_Click(sender,e);
+            }
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+    }
+}
